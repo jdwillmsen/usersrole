e4af05dda6cce4d61754cc128053c9d9b2ae14c5
Jake Willmsen <jdwillmsen@gmail.com>
2023-05-29 21:52:37 -0500

    firebase workflow setup for enviroment files

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const dir = 'src/environments';
+const file = 'environment.ts';
+const prodFile = 'environment.production.ts';
+
+const content = `${process.env.FIREBASE}`;
+
+fs.access(dir, fs.constants.F_OK, (err) => {
+  if (err) {
+    // Directory doesn't exist
+    console.log("src doesn't exist, creating now", process.cwd());
+    // Create /src
+    fs.mkdir(dir, { recursive: true }, (err) => {
+      if (err) throw err;
+    });
+  }
+  // Now write to file
+  try {
+    fs.writeFileSync(dir + '/' + file, content);
+    fs.writeFileSync(dir + '/' + prodFile, content);
+    console.log('Created successfully in', process.cwd());
+    if (fs.existsSync(dir + '/' + file)) {
+      console.log('File is created', path.resolve(dir + '/' + file));
+      const str = fs.readFileSync(dir + '/' + file).toString();
+      console.log(str);
+    }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+});
