5d23516735fedb6b4eba4b8de9d419d7fff9af1b
Jake Willmsen <jdwillmsen@gmail.com>
2023-06-07 00:27:49 -0500

    Added firebase functions to begin role based implementation

---
+++ functions/src/auth/authorized.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { Request, Response } from 'express';
+
+export function isAuthorized(opts: {
+  hasRole: Array<'admin' | 'manager' | 'user'>;
+  allowSameUser?: boolean;
+}) {
+  return (req: Request, res: Response, next: Function) => {
+    const { role, email, uid } = res.locals;
+    const { id } = req.params;
+
+    if (email === 'your-root-user-email@domain.com') {
+      return next();
+    }
+
+    if (opts.allowSameUser && id && uid === id) {
+      return next();
+    }
+
+    if (!role) {
+      return res.status(403).send();
+    }
+
+    if (opts.hasRole.includes(role)) {
+      return next();
+    }
+
+    return res.status(403).send();
+  };
+}
---
+++ functions/src/users/routes-config.ts
@@ -0,0 +1,38 @@
+import { Application } from 'express';
+import { remove, patch, get, all, create } from './controller';
+import { isAuthenticated } from '../auth/authenticated';
+import { isAuthorized } from '../auth/authorized';
+
+export function routesConfig(app: Application) {
+  // creates user
+  app.post(
+      '/users',
+      isAuthenticated,
+      isAuthorized({ hasRole: ['admin', 'manager'] }),
+      create
+  );
+  // lists all users
+  app.get('/users', [
+    isAuthenticated,
+    isAuthorized({ hasRole: ['admin', 'manager'] }),
+    all
+  ]);
+  // get :id user
+  app.get('/users/:id', [
+    isAuthenticated,
+    isAuthorized({ hasRole: ['admin', 'manager'], allowSameUser: true }),
+    get
+  ]);
+  // updates :id user
+  app.patch('/users/:id', [
+    isAuthenticated,
+    isAuthorized({ hasRole: ['admin', 'manager'], allowSameUser: true }),
+    patch
+  ]);
+  // deletes :id user
+  app.delete('/users/:id', [
+    isAuthenticated,
+    isAuthorized({ hasRole: ['admin', 'manager'] }),
+    remove
+  ]);
+}
