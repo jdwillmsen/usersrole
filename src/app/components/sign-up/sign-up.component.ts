36b60ccf0d091bfc27e5f988ad2e65f3db97c93a
Jake Willmsen <jdwillmsen@gmail.com>
2023-07-09 01:32:26 -0500

    Added sign up validation and proper error messages for signUp form

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,16 +14,61 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class SignUpComponent {
   hide = true;
   signUpForm = new FormGroup({
-    email: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     displayName: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-    confirmPassword: new FormControl('', [Validators.required])
+    matchingPassword: new FormGroup(
+      {
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6)
+        ]),
+        confirmPassword: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6)
+        ])
+      },
+      { validators: this.passwordMatch() }
+    )
   });
+  validationMessages = {
+    email: [
+      { type: 'required', message: 'Email is required' },
+      { type: 'email', message: 'Enter a valid email' }
+    ],
+    displayName: [{ type: 'required', message: 'Display name is required' }],
+    password: [
+      { type: 'required', message: 'Password is required' },
+      {
+        type: 'minlength',
+        message: 'Password must be at least 6 characters long'
+      }
+    ],
+    confirmPassword: [
+      { type: 'required', message: 'Confirm password is required' },
+      {
+        type: 'minlength',
+        message: 'Password must be at least 6 characters long'
+      },
+      { type: 'passwordMatch', message: 'Password mismatch' }
+    ]
+  };
+
   signUp() {
     if (this.signUpForm.valid) {
       alert('Sign Up Presed');
     } else {
       alert('Sign Up Form Error');
     }
+
+    console.log(this.signUpForm.value);
+  }
+
+  passwordMatch(): ValidatorFn {
+    return () => {
+      const matches =
+        this.signUpForm?.controls.matchingPassword.get('password')?.value ===
+        this.signUpForm?.controls.matchingPassword.get('confirmPassword')?.value;
+      return !matches ? { passwordMatch: true } : null;
+    };
   }
 }
