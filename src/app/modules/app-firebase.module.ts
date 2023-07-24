ce3d164aff2d7c5314e669c4de22857c6b8bc661
Jake Willmsen <jdwillmsen@gmail.com>
2023-07-23 22:08:56 -0500

    Add firestore module

--- src/app/modules/app-firebase.module.ts
+++ src/app/modules/app-firebase.module.ts
@@ -1,11 +1,16 @@
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireModule } from '@angular/fire/compat';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { NgModule } from '@angular/core';
 
 import { environment } from 'src/environments/environment';
 
 @NgModule({
-  imports: [AngularFireModule.initializeApp(environment.firebase)],
-  exports: [AngularFireModule, AngularFireAuthModule,]
+  imports: [
+    AngularFireAuthModule,
+    provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  exports: [AngularFireModule, AngularFireAuthModule]
 })
 export class AppFirebaseModule {}
