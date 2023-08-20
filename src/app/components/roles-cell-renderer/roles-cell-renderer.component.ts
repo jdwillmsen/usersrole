4cc1821a7a450b7b5e0b2b2b8331044c9299be7b
Jake Willmsen <jdwillmsen@gmail.com>
2023-08-20 17:09:37 -0500

    Added ag grid to users page and refactored the users form actions associated with it

---
+++ src/app/components/roles-cell-renderer/roles-cell-renderer.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { ICellRendererAngularComp } from 'ag-grid-angular';
+import { ICellRendererParams } from 'ag-grid-community';
+import { Role } from 'src/app/models/users.model';
+
+@Component({
+  selector: 'app-roles-cell-renderer',
+  templateUrl: './roles-cell-renderer.component.html',
+  styleUrls: ['./roles-cell-renderer.component.scss']
+})
+export class RolesCellRendererComponent implements ICellRendererAngularComp {
+  roles!: Role[];
+
+  agInit(params: ICellRendererParams): void {
+    this.roles = params.value;
+  }
+
+  refresh() {
+    return false;
+  }
+}
