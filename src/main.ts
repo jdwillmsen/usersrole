176c0816565e40710e2beff5ad081ac9d81f537c
Jake Willmsen <jdwillmsen@gmail.com>
2023-08-30 19:29:30 -0500

    Converted the root module into a standalone component and made use of bootstrap api

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,31 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ThemeModule } from './app/theme/theme.module';
+import { TestingModule } from './app/testing/testing.module';
+import { ProfileModule } from './app/profile/profile.module';
+import { HomeModule } from './app/home/home.module';
+import { AuthenticationModule } from './app/authentication/authentication.module';
+import { AdminModule } from './app/admin/admin.module';
+import { CoreModule } from './app/core/core.module';
+import { AppRoutingModule } from './app/app-routing.module';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      CoreModule,
+      AdminModule,
+      AuthenticationModule,
+      HomeModule,
+      ProfileModule,
+      TestingModule,
+      ThemeModule,
+      MatSnackBarModule
+    ),
+    provideAnimations()
+  ]
+}).catch((err) => console.error(err));
