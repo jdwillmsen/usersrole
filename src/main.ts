ca5e6f4f059b8f46c154117c2a735e2c31e004ab
Jake Willmsen <jdwillmsen@gmail.com>
2023-09-02 20:30:36 -0500

    Converting app to a PWA

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ThemeModule } from './app/theme/theme.module';
@@ -11,21 +11,15 @@ import { CoreModule } from './app/core/core.module';
 import { AppRoutingModule } from './app/app-routing.module';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(
-      BrowserModule,
-      AppRoutingModule,
-      CoreModule,
-      AdminModule,
-      AuthenticationModule,
-      HomeModule,
-      ProfileModule,
-      TestingModule,
-      ThemeModule,
-      MatSnackBarModule
-    ),
-    provideAnimations()
-  ]
+    importProvidersFrom(BrowserModule, AppRoutingModule, CoreModule, AdminModule, AuthenticationModule, HomeModule, ProfileModule, TestingModule, ThemeModule, MatSnackBarModule),
+    provideAnimations(),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 }).catch((err) => console.error(err));
