4c830467598d9af52ada73e53c27ea365ed0b27e
Jake Willmsen <jdwillmsen@gmail.com>
2023-09-04 20:15:28 -0500

    Extract date functions out of class

--- src/app/admin/components/users/users.component.ts
+++ src/app/admin/components/users/users.component.ts
@@ -22,6 +22,44 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [MatButtonModule, MatIconModule, AgGridModule, AsyncPipe]
 })
 export class UsersComponent implements OnInit {
+  static columnDefs: ColDef[] = [
+    { field: 'uid', cellDataType: 'text' },
+    { field: 'email', cellDataType: 'text' },
+    { field: 'displayName', cellDataType: 'text' },
+    {
+      field: 'roles',
+      cellRenderer: RolesCellRendererComponent,
+      cellDataType: 'text'
+    },
+    {
+      field: 'lastSignInTime',
+      filter: 'agDateColumnFilter',
+      filterParams: {
+        comparator: dateFilterComparator
+      },
+      comparator: dateSortComparator,
+      cellDataType: 'text'
+    },
+    {
+      field: 'creationTime',
+      filter: 'agDateColumnFilter',
+      filterParams: {
+        comparator: dateFilterComparator
+      },
+      comparator: dateSortComparator,
+      cellDataType: 'text'
+    },
+    {
+      field: 'actions',
+      autoHeight: true,
+      cellRenderer: ActionsButtonCellRendererComponent,
+      maxWidth: 180,
+      minWidth: 180,
+      resizable: false,
+      filter: false,
+      sortable: false
+    }
+  ];
   users$!: Observable<User[]>;
   defaultColDef: ColDef = {
     sortable: true,
@@ -50,69 +88,6 @@ export class UsersComponent implements OnInit {
     private userFormService: UserFormService,
     private snackbarService: SnackbarService
   ) {}
-
-  private static dateSortComparator = (
-    stringDate1: string,
-    stringDate2: string
-  ) => {
-    const date1 = new Date(stringDate1);
-    const date2 = new Date(stringDate2);
-    if (date1 === null && date2 === null) return 0;
-    if (date1 === null) return -1;
-    if (date2 === null) return 1;
-    return date1.getTime() - date2.getTime();
-  };
-
-  private static dateFilterComparator = (
-    filterLocalDateAtMidnight: Date,
-    cellValue: string
-  ) => {
-    if (cellValue === null) return -1;
-    const cellDate = new Date(cellValue);
-    if (filterLocalDateAtMidnight.getTime() === cellDate.getTime()) return 0;
-    if (cellDate < filterLocalDateAtMidnight) return -1;
-    if (cellDate > filterLocalDateAtMidnight) return 1;
-    return 0;
-  };
-
-  static columnDefs: ColDef[] = [
-    { field: 'uid', cellDataType: 'text' },
-    { field: 'email', cellDataType: 'text' },
-    { field: 'displayName', cellDataType: 'text' },
-    {
-      field: 'roles',
-      cellRenderer: RolesCellRendererComponent,
-      cellDataType: 'text'
-    },
-    {
-      field: 'lastSignInTime',
-      filter: 'agDateColumnFilter',
-      filterParams: {
-        comparator: UsersComponent.dateFilterComparator
-      },
-      comparator: UsersComponent.dateSortComparator,
-      cellDataType: 'text'
-    },
-    {
-      field: 'creationTime',
-      filter: 'agDateColumnFilter',
-      filterParams: {
-        comparator: UsersComponent.dateFilterComparator
-      },
-      comparator: UsersComponent.dateSortComparator,
-      cellDataType: 'text'
-    },
-    {
-      field: 'actions',
-      autoHeight: true,
-      cellRenderer: ActionsButtonCellRendererComponent,
-      maxWidth: 180,
-      minWidth: 180,
-      resizable: false,
-      filter: false,
-      sortable: false
-    }
-  ];
 
   ngOnInit() {
     this.users$ = this.userService.users$;
@@ -145,4 +120,25 @@ export class UsersComponent implements OnInit {
       }
     });
   }
+}
+
+function dateFilterComparator(
+  filterLocalDateAtMidnight: Date,
+  cellValue: string
+) {
+  if (cellValue === null) return -1;
+  const cellDate = new Date(cellValue);
+  if (filterLocalDateAtMidnight.getTime() === cellDate.getTime()) return 0;
+  if (cellDate < filterLocalDateAtMidnight) return -1;
+  if (cellDate > filterLocalDateAtMidnight) return 1;
+  return 0;
+}
+
+function dateSortComparator(stringDate1: string, stringDate2: string) {
+  const date1 = new Date(stringDate1);
+  const date2 = new Date(stringDate2);
+  if (date1 === null && date2 === null) return 0;
+  if (date1 === null) return -1;
+  if (date2 === null) return 1;
+  return date1.getTime() - date2.getTime();
 }
