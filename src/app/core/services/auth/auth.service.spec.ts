5de78eed775846dfc8af550eaefd709da28b902f
Jake Willmsen <jdwillmsen@gmail.com>
2023-09-07 20:55:01 -0500

    Enhanced authService unit testing

--- src/app/core/services/auth/auth.service.spec.ts
+++ src/app/core/services/auth/auth.service.spec.ts
@@ -1,30 +1,31 @@
 import { AuthService } from './auth.service';
 import { expect } from '@jest/globals';
 import { of } from 'rxjs';
+import { GoogleAuthProvider } from 'firebase/auth';
 
 describe('AuthService', () => {
   let authService: AuthService;
-  let angularFireAuthMock: jest.Mocked<any>;
-  let routerMock: jest.Mocked<any>;
-  let snackbarServiceMock: jest.Mocked<any>;
+  const angularFireAuthMock: jest.Mocked<any> = {
+    authState: of(null),
+    signInWithEmailAndPassword: jest.fn(),
+    signInWithPopup: jest.fn(),
+    signOut: jest.fn()
+  };
+  const routerMock: jest.Mocked<any> = {
+    navigate: jest.fn()
+  };
+  const snackbarServiceMock: jest.Mocked<any> = {
+    success: jest.fn(),
+    error: jest.fn()
+  };
+  const authProviderMock = new GoogleAuthProvider();
+  const defaultEmail = 'testUser@usersrole.com';
+  const defaultPassword = 'testPassword';
+  const defaultErrorMessage = 'Authentication Error';
+  const defaultLoginSuccessMessage = 'Login Successful';
+  const defaultLogoutSuccessMessage = 'Logout Successful';
 
   beforeEach(() => {
-    angularFireAuthMock = {
-      authState: of(null),
-      signInWithEmailAndPassword: jest.fn(),
-      signInWithPopup: jest.fn(),
-      signOut: jest.fn()
-    };
-
-    routerMock = {
-      navigate: jest.fn()
-    };
-
-    snackbarServiceMock = {
-      success: jest.fn(),
-      error: jest.fn()
-    };
-
     authService = new AuthService(
       angularFireAuthMock,
       routerMock,
@@ -34,5 +35,141 @@ describe('AuthService', () => {
 
   it('should create an instance of AuthService', () => {
     expect(authService).toBeInstanceOf(AuthService);
+  });
+
+  it('should call emailAuth and navigate to home on success', (done) => {
+    angularFireAuthMock.signInWithEmailAndPassword.mockResolvedValueOnce({});
+
+    authService.emailAuth(defaultEmail, defaultPassword).subscribe(() => {
+      expect(
+        angularFireAuthMock.signInWithEmailAndPassword
+      ).toHaveBeenCalledWith(defaultEmail, defaultPassword);
+      expect(snackbarServiceMock.success).toHaveBeenCalledWith(
+        defaultLoginSuccessMessage,
+        { variant: 'filled', autoClose: true },
+        true
+      );
+      expect(routerMock.navigate).toHaveBeenCalledWith(['home']);
+      done();
+    });
+  });
+
+  it('should call emailAuth and show error snackbar on failure', (done) => {
+    angularFireAuthMock.signInWithEmailAndPassword.mockRejectedValueOnce({
+      message: defaultErrorMessage
+    });
+
+    authService.emailAuth(defaultEmail, defaultPassword).subscribe(() => {
+      expect(
+        angularFireAuthMock.signInWithEmailAndPassword
+      ).toHaveBeenCalledWith(defaultEmail, defaultPassword);
+      expect(snackbarServiceMock.error).toHaveBeenCalledWith(
+        defaultErrorMessage,
+        { variant: 'filled' },
+        true
+      );
+      done();
+    });
+  });
+
+  it('should call googleAuth and navigate to home on success', (done) => {
+    angularFireAuthMock.signInWithPopup.mockResolvedValueOnce({});
+
+    authService.googleAuth().subscribe(() => {
+      expect(angularFireAuthMock.signInWithPopup).toHaveBeenCalledWith(
+        expect.any(GoogleAuthProvider)
+      );
+      expect(snackbarServiceMock.success).toHaveBeenCalledWith(
+        defaultLoginSuccessMessage,
+        { variant: 'filled', autoClose: true },
+        true
+      );
+      expect(routerMock.navigate).toHaveBeenCalledWith(['home']);
+      done();
+    });
+  });
+
+  it('should call googleAuth and show error snackbar on failure', (done) => {
+    angularFireAuthMock.signInWithPopup.mockRejectedValueOnce({
+      message: defaultErrorMessage
+    });
+
+    authService.googleAuth().subscribe(() => {
+      expect(angularFireAuthMock.signInWithPopup).toHaveBeenCalledWith(
+        expect.any(GoogleAuthProvider)
+      );
+      expect(snackbarServiceMock.error).toHaveBeenCalledWith(
+        defaultErrorMessage,
+        { variant: 'filled' },
+        true
+      );
+      done();
+    });
+  });
+
+  it('should call authLogin and navigate to home on success', (done) => {
+    angularFireAuthMock.signInWithPopup.mockResolvedValueOnce({});
+
+    authService.authLogin(authProviderMock).subscribe(() => {
+      expect(angularFireAuthMock.signInWithPopup).toHaveBeenCalledWith(
+        authProviderMock
+      );
+      expect(snackbarServiceMock.success).toHaveBeenCalledWith(
+        defaultLoginSuccessMessage,
+        { variant: 'filled', autoClose: true },
+        true
+      );
+      expect(routerMock.navigate).toHaveBeenCalledWith(['home']);
+      done();
+    });
+  });
+
+  it('should call authLogin and show error snackbar on failure', (done) => {
+    angularFireAuthMock.signInWithPopup.mockRejectedValueOnce({
+      message: defaultErrorMessage
+    });
+
+    authService.authLogin(authProviderMock).subscribe(() => {
+      expect(angularFireAuthMock.signInWithPopup).toHaveBeenCalledWith(
+        authProviderMock
+      );
+      expect(snackbarServiceMock.error).toHaveBeenCalledWith(
+        defaultErrorMessage,
+        { variant: 'filled' },
+        true
+      );
+      done();
+    });
+  });
+
+  it('should call authLogout and navigate to sign-in on success', (done) => {
+    angularFireAuthMock.signOut.mockResolvedValueOnce({});
+
+    authService.authLogout().subscribe(() => {
+      expect(angularFireAuthMock.signOut).toHaveBeenCalled();
+      expect(snackbarServiceMock.success).toHaveBeenCalledWith(
+        defaultLogoutSuccessMessage,
+        { variant: 'filled', autoClose: true },
+        true
+      );
+      expect(routerMock.navigate).toHaveBeenCalledWith(['sign-in']);
+      done();
+    });
+  });
+
+  it('should call authLogout and show error snackbar on failure', (done) => {
+    angularFireAuthMock.signOut.mockRejectedValueOnce({
+      message: defaultErrorMessage
+    });
+
+    authService.authLogout().subscribe(() => {
+      expect(angularFireAuthMock.signOut).toHaveBeenCalled();
+      expect(snackbarServiceMock.error).toHaveBeenCalledWith(
+        defaultErrorMessage,
+        { variant: 'filled' },
+        true
+      );
+      done();
+    });
   });
 });
