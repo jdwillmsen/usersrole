0bdb0e757c69693f11eba23bfc87a167ae0f1b47
Jake Willmsen <jdwillmsen@gmail.com>
2023-09-10 23:49:24 -0500

    Enhanced auth token interceptor unit tests

--- src/app/core/interceptors/auth-token.interceptor.spec.ts
+++ src/app/core/interceptors/auth-token.interceptor.spec.ts
@@ -1,27 +1,54 @@
 import { TestBed } from '@angular/core/testing';
-import { AuthTokenInterceptor } from './auth-token.interceptor';
+import {
+  AuthTokenHttpInterceptorProvider,
+  AuthTokenInterceptor
+} from './auth-token.interceptor';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { of } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { expect } from '@jest/globals';
+import { HttpRequest } from '@angular/common/http';
 
 describe('AuthTokenInterceptor', () => {
-  beforeEach(() =>
+  let interceptor: AuthTokenInterceptor;
+  let angularFireAuthMock: jest.Mocked<any> = {
+    idToken: new BehaviorSubject<string | null>('mocked-id-token')
+  };
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         AuthTokenInterceptor,
         {
           provide: AngularFireAuth,
-          useValue: {
-            idToken: of('mockedToken')
-          }
-        }
+          useValue: angularFireAuthMock
+        },
+        AuthTokenHttpInterceptorProvider
       ]
-    })
-  );
+    });
 
-  it('should be created', () => {
-    const interceptor: AuthTokenInterceptor =
-      TestBed.inject(AuthTokenInterceptor);
-    expect(interceptor).toBeTruthy();
+    interceptor = TestBed.inject(AuthTokenInterceptor);
+    angularFireAuthMock = TestBed.inject(AngularFireAuth);
+  });
+
+  it('should create an instance of AuthTokenInterceptor', () => {
+    expect(interceptor).toBeInstanceOf(AuthTokenInterceptor);
+  });
+
+  it('should intercept requests and add an Authorization header with the idToken', (done) => {
+    const request = new HttpRequest('GET', 'https://example.com/api/data');
+
+    interceptor
+      .intercept(request, {
+        handle: (req: HttpRequest<unknown>) => {
+          expect(req.headers.get('Authorization')).toEqual(
+            'Bearer mocked-id-token'
+          );
+          done();
+          return new Observable();
+        }
+      })
+      .subscribe();
+
+    angularFireAuthMock.idToken.next('mocked-id-token');
   });
 });
