2b76863dc0c36bdbf5d13976aab5a3043b4b3a6e
Jake Willmsen <jdwillmsen@gmail.com>
2023-09-21 23:04:49 -0500

    Enhanced route-tile component tests

--- src/app/home/components/route-tile/route-tile.component.cy.ts
+++ src/app/home/components/route-tile/route-tile.component.cy.ts
@@ -12,4 +12,43 @@ describe('TileComponent', () => {
       ]
     });
   });
+
+  it('should have default set', () => {
+    cy.mount(RouteTileComponent, {
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {}
+        }
+      ]
+    });
+    cy.getByCy('title').should('have.text', '');
+    cy.getByCy('message').should('have.text', '');
+    cy.getByCy('access').should('be.visible').and('contain.text', 'Access:');
+  });
+
+  it('should be setup properly', () => {
+    cy.mount(RouteTileComponent, {
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {}
+        }
+      ],
+      componentProperties: {
+        title: 'Test',
+        link: '/test',
+        description: 'test description',
+        footer: 'tester'
+      }
+    });
+    cy.getByCy('title').should('be.visible').and('contain.text', 'Test');
+    cy.getByCy('message')
+      .should('be.visible')
+      .and('contain.text', 'test description');
+    cy.getByCy('access').should('be.visible').and('contain.text', 'tester');
+    cy.getByCy('accessTitle')
+      .should('be.visible')
+      .and('contain.text', 'Access:');
+  });
 });
