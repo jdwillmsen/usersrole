e06e863e850e557cdac5cfcf93a2bd223a4b7344
Jake Willmsen <jdwillmsen@gmail.com>
2023-09-21 19:03:47 -0500

    Enhanced forbidden component tests

--- src/app/authentication/components/forbidden/forbidden.component.cy.ts
+++ src/app/authentication/components/forbidden/forbidden.component.cy.ts
@@ -12,4 +12,27 @@ describe('ForbiddenComponent', () => {
       ]
     });
   });
+
+  it('should be setup properly', () => {
+    cy.mount(ForbiddenComponent, {
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {}
+        }
+      ]
+    });
+    cy.getByCy('number').should('be.visible').and('have.text', '403');
+    cy.getByCy('title').should('be.visible').and('have.text', 'Forbidden');
+    cy.getByCy('message')
+      .should('be.visible')
+      .and(
+        'contain.text',
+        'You do not have permission to access this resource.'
+      );
+    cy.getByCy('homeButton')
+      .should('be.visible')
+      .and('contain.text', 'Home')
+      .and('be.enabled');
+  });
 });
