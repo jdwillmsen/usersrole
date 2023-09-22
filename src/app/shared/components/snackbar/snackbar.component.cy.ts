769a77321a5a35d52a43afb90bf0fd9583f7ef72
Jake Willmsen <jdwillmsen@gmail.com>
2023-09-22 00:48:39 -0500

    Enhanced snackbar component tests

--- src/app/shared/components/snackbar/snackbar.component.cy.ts
+++ src/app/shared/components/snackbar/snackbar.component.cy.ts
@@ -20,4 +20,57 @@ describe('SnackbarComponent', () => {
       ]
     });
   });
+
+  it('should be setup properly with no button text', () => {
+    cy.mount(SnackbarComponent, {
+      imports: [],
+      providers: [
+        {
+          provide: MatSnackBarRef,
+          useValue: {}
+        },
+        {
+          provide: MAT_SNACK_BAR_DATA,
+          useValue: {
+            icon: 'home',
+            message: 'This is a component test',
+            buttonText: undefined
+          }
+        }
+      ]
+    });
+    cy.getByCy('icon').should('be.visible');
+    cy.getByCy('message')
+      .should('be.visible')
+      .and('contain.text', 'This is a component test');
+    cy.getByCy('close-button').should('be.visible').and('be.enabled');
+  });
+
+  it('should be setup properly with button text', () => {
+    cy.mount(SnackbarComponent, {
+      imports: [],
+      providers: [
+        {
+          provide: MatSnackBarRef,
+          useValue: {}
+        },
+        {
+          provide: MAT_SNACK_BAR_DATA,
+          useValue: {
+            icon: 'home',
+            message: 'This is a component test',
+            buttonText: 'Test'
+          }
+        }
+      ]
+    });
+    cy.getByCy('icon').should('be.visible');
+    cy.getByCy('message')
+      .should('be.visible')
+      .and('contain.text', 'This is a component test');
+    cy.getByCy('close-button')
+      .should('be.visible')
+      .and('be.enabled')
+      .and('contain.text', 'Test');
+  });
 });
