ef9d1d77e10b3d04476b0ed7ebea06dbebd24e85
Jake Willmsen <jdwillmsen@gmail.com>
2023-09-21 22:37:22 -0500

    Enhanced home component tests

--- src/app/home/components/home/home.component.cy.ts
+++ src/app/home/components/home/home.component.cy.ts
@@ -12,4 +12,30 @@ describe('HomeComponent', () => {
       ]
     });
   });
+
+  it('should be setup properly', () => {
+    cy.mount(HomeComponent, {
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {}
+        }
+      ]
+    });
+    cy.getByCy('title').should('be.visible').and('contain.text', 'Users Role');
+    cy.getByCy('p1').should('be.visible').and('not.be.empty');
+    cy.getByCy('p2').should('be.visible').and('not.be.empty');
+    cy.getByCy('p3').should('be.visible').and('not.be.empty');
+    cy.getByCy('p4').should('be.visible').and('not.be.empty');
+    cy.getByCy('readMessage').should('be.visible').and('not.be.empty');
+    cy.getByCy('tileHome').should('be.visible');
+    cy.getByCy('tileProfile').should('be.visible');
+    cy.getByCy('tileAlerts').should('be.visible');
+    cy.getByCy('tileSnackbars').should('be.visible');
+    cy.getByCy('tileButtons').should('be.visible');
+    cy.getByCy('tilePalettes').should('be.visible');
+    cy.getByCy('tileTheme').should('be.visible');
+    cy.getByCy('tileUsers').should('be.visible');
+    cy.getByCy('tileRoles').should('be.visible');
+  });
 });
