9d6369652776327844acaa57ce18eec05628a075
Jake Willmsen <jdwillmsen@gmail.com>
2023-09-24 17:59:45 -0500

    Enhanced app component tests

---
+++ src/app/app.component.cy.ts
@@ -0,0 +1,26 @@
+import { AppComponent } from './app.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+
+describe('AppComponent', () => {
+  beforeEach(() => {
+    cy.mount(AppComponent, {
+      imports: [
+        MatSnackBarModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        HttpClientModule,
+        AppRoutingModule
+      ]
+    });
+  });
+
+  it('should mount', () => {});
+
+  it('should be setup properly', () => {
+    cy.getByCy('header').should('be.visible');
+    cy.getByCy('main').should('be.visible');
+  });
+});
---
+++ src/app/app.config.ts
@@ -0,0 +1,39 @@
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode
+} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+import { AdminModule } from './admin/admin.module';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { HomeModule } from './home/home.module';
+import { ProfileModule } from './profile/profile.module';
+import { TestingModule } from './testing/testing.module';
+import { ThemeModule } from './theme/theme.module';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      CoreModule,
+      AdminModule,
+      AuthenticationModule,
+      HomeModule,
+      ProfileModule,
+      TestingModule,
+      ThemeModule,
+      MatSnackBarModule
+    ),
+    provideAnimations(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ]
+};
